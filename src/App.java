4e38966f4e923136075a1fb9534e6fd6badc5de6
HenryZisow <28hzisow@gannacademy.org>
2026-01-16 12:33:57 -0500

    Done

--- src/App.java
+++ src/App.java
@@ -1,15 +1,19 @@
 public class App {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int[][] a = {
-			{5,2,3,7},
-			{1,5,2,2},
-			{1,2,3,4}
+				{ 5, 2, 3, 7 },
+				{ 1, 5, 2, 2 },
+				{ 1, 2, 3, 4 }
 		};
-		System.out.println(sumRow(a,2)); //should print 10
-		System.out.println(sumRow(a,0)); //should print 17
+		System.out.println(sumRow(a, 2)); // should print 10
+		System.out.println(sumRow(a, 0)); // should print 17
 	}
-	public static int sumRow(int[][] array, int row){
-		return -1;
+
+	public static int sumRow(int[][] array, int row) {
+		int sum = 0;
+		for (int i = 0; i < array[row].length; i++) {
+			sum = sum + array[row][i];
+		}
+		return sum;
 	}
-	
 }
